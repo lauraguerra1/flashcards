0692630c5951894280c9ffab73b763516a6a50b3
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-08 21:28:40 -0400

    Fix before each hook

--- test/deck-test.js
+++ test/deck-test.js
@@ -6,23 +6,23 @@ const sample = require('../src/sample-data');
 
 describe('deck', function() {
   beforeEach(function () {
-    const card1 = createCard(sample.ids[0], sample.questions[0], sample.answers[0], sample.correctAnswers[0]);
-    const card2 = createCard(sample.ids[1], sample.questions[1], sample.answers[1], sample.correctAnswers[1]);
-    const card3 = createCard(sample.ids[2], sample.questions[2], sample.answers[2], sample.correctAnswers[2]);
-    const deck = createDeck([card1, card2, card3]);
-    it('should be a function', function() {
-      assert.isFunction(createDeck);
-    })
-    it('should create a deck of cards', function () {
-      assert.deepEqual(deck.cards, [card1, card2])
-    })
-    it('should know how many cards are in the deck', function() {
-      const deck2 = createDeck([]);
-      const cardCount = countCards(deck);
-      const cardCount2 = countCards(deck2);
+    this.card1 = createCard(sample.ids[0], sample.questions[0], sample.answers[0], sample.correctAnswers[0]);
+    this.card2 = createCard(sample.ids[1], sample.questions[1], sample.answers[1], sample.correctAnswers[1]);
+    this.card3 = createCard(sample.ids[2], sample.questions[2], sample.answers[2], sample.correctAnswers[2]);
+    this.deck = createDeck([this.card1, this.card2, this.card3]);
+  })
+  it('should be a function', function() {
+    assert.isFunction(createDeck);
+  })
+  it('should create a deck of cards', function () {
+    assert.deepEqual(this.deck.cards, [this.card1, this.card2, this.card3])
+  })
+  it('should know how many cards are in the deck', function() {
+    const deck2 = createDeck([]);
+    const cardCount = countCards(this.deck);
+    const cardCount2 = countCards(deck2);
       
-      assert.equal(cardCount, 3);
-      assert.equal(cardCount2, 0);
-    })
+    assert.equal(cardCount, 3);
+    assert.equal(cardCount2, 0);
   })
 })
