c25b4c51571fb581b54c593c19b948c38fa0192a
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-08 21:07:40 -0400

    Change to chai.assert and use sample data

---
+++ src/sample-data.js
@@ -0,0 +1,8 @@
+const sample = {
+  ids: [1, 2, 3],
+  questions: ['What allows you to define a set of related information using key-value pairs?', 'What is Laura\'s Golden Retriever\'s name?', 'What mod is Laura in?'],
+  answers: [['object', 'array', 'function'], ['Bruce', 'Jackson', 'Ducky'], [1, 2, 3, 4]],
+  correctAnswers: ['object', 'Jackson', 2]
+}
+
+module.exports = sample ;
