ecf618d3e22065d9b924135f42c9e62a9deff465
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-08 22:51:27 -0400

    Fix exports and use createCards in .then method

--- src/card.js
+++ src/card.js
@@ -7,19 +7,7 @@ const createCard = (id, question, answers, correctAnswer) => {
   };
 };
 
-const createDeck = (data) => {
-  return {
-    cards: data
-  }
-};
-
-const countCards = (deck) => {
-  const cardCount = deck.cards.length;
-  return cardCount;
-}
 
 module.exports = {
   createCard,
-  createDeck,
-  countCards
 };
